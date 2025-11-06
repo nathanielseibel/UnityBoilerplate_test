81b72a9551a14790c690dec03c342ac4bdeff247
Nate Seibel <nathaniel.seibel@mymail.champlain.edu>
2025-11-06 11:59:49 -0500

    Game over screen (rudimentary) implemented, score carries over

--- Assets/Scripts/BaseManager.cs
+++ Assets/Scripts/BaseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BaseManager : MonoBehaviour
 {
@@ -51,6 +52,7 @@ public class BaseManager : MonoBehaviour
                 //report to GM
                 //destroy raft LAST
                 Destroy(gameObject);
+                SceneManager.LoadScene("GameOver");
             }
         }
     }
