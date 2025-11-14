b22460c37268e1a776cc6cf5a2d68f4e900a0057
Nate Seibel <nathaniel.seibel@mymail.champlain.edu>
2025-11-13 20:48:54 -0500

    Forgot to add story scene to build
    Fixed that, also changed the name of my story scene to avoid confusion

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Scenes/Story and Tut");
+        SceneManager.LoadScene("Story and Tut");
         //Time starts
         Time.timeScale = 1.0f;
     }
